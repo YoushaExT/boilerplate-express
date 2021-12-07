a85371b61bb5ca8813d7ff95f06942ea9d5dcece
Yousha Arshad <yousharunescape@yahoo.com>
2021-12-07 14:44:23 +0500

    remove empty spaces and console.log

--- myApp.js
+++ myApp.js
@@ -1,30 +1,12 @@
 var express = require('express');
 var app = express();
 
-console.log("Hello World")
 
 mainPage = (req, res) => {
 	res.send("Hello Express")
 }
 
-
 app.get('/', mainPage)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
  module.exports = app;
